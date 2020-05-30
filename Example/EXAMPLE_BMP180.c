bfbdedc44f22ce7471f3c93729088c688521519f
mjx4mois <mjx4mois@gmail.com>
2020-05-30 13:35:02 +0800

    update the source code / project code remove porting_layer.c/.h add swi2c_layer.c/.h add datatype_layer.h

--- Example/EXAMPLE_BMP180.c
+++ Example/EXAMPLE_BMP180.c
@@ -37,119 +37,121 @@ void EXAMPLE_BMP180(void)
 			I2C register address 0xAA~0xBF */
 		BMP180_GET_CAL_COEFF();
 
-		/* Get the Temperature & Pressure */
-		check_flag_BMP180 = BMP180_GET_TEMP_PRESS(&final_temperature,&final_pressure,OVERSAMPLING);
-		
-		/* Calculate the altitude */		
-		BMP180_GET_ALTITUDE(final_pressure,&final_altitude);
-
-		/* Calculate the sea level pressure */
-		BMP180_GET_SEALEVEL_PRESSURE(final_altitude,final_pressure,&final_sealevel_press);
-
-
-		/* print result */
-		if(check_flag_BMP180==0)
+		while(1)
 		{
-                    printf("Temperature : %d.%d C\r\n",final_temperature/10,final_temperature%10);
-                    printf("Pressure    : %ld Pa (%ld.%ld hPa)\r\n",(INT32S)final_pressure,(INT32S)final_pressure/100,(INT32S)final_pressure%100);
-                    printf("Altitude    : %f m\r\n",final_altitude);
-                    printf("Sea leve press   : %f Pa\r\n",final_sealevel_press);
-                    printf("--------------------BMP180--------------------\r\n\r\n");
-
-
-            		 /* BMP180 temperature */                    
-	            	lcd_gotoxy(0,0);
-	            	lcd_putsf("Temp:");     
-
-	           	if(final_temperature<0)
-	            	{
-	            		lcd_putchar(45);
-			}                             
-	            	else
-			{
-				lcd_putchar(32);
-			}
-					
-			/* final_temperature unit : 0.1 C -> if  final_temperature == 245 , 24.5 C */
- 			lcd_temp[0]=final_temperature/100;
-			lcd_temp[1]=(final_temperature/10)%10;
-			lcd_temp[2]=final_temperature%10;
-			/* show final_temperature data on LCD */
-			lcd_putchar(48+lcd_temp[0]);
-		       lcd_putchar(48+lcd_temp[1]);
-			lcd_putchar(46);	/* show "."*/	
-		       lcd_putchar(48+lcd_temp[2]);
-		       lcd_putchar(67);	/* show "C"*/	
-
-
-            		 /* BMP085 Pressure */                    
-	            	lcd_gotoxy(0,1);
-	            	lcd_putsf("P:");    
-			/* final_pressure unit : Pa -> if  final_pressure == 99687 Pa , 996.87hPa */
-			lcd_data[0]=((INT32S)final_pressure/100000);
-			lcd_data[1]=((INT32S)final_pressure/10000)%10;
-			lcd_data[2]=((INT32S)final_pressure/1000)%10;
-			lcd_data[3]=((INT32S)final_pressure/100)%10;	
-			lcd_data[4]=((INT32S)final_pressure/10)%10;
-			lcd_data[5]=((INT32S)final_pressure)%10;
-			/* show final_pressure data on LCD */
-			lcd_putchar(48+lcd_data[0]);
-			lcd_putchar(48+lcd_data[1]);
-			lcd_putchar(48+lcd_data[2]);
-		       lcd_putchar(48+lcd_data[3]);
-			lcd_putchar(46);	/* show "."*/	
-		       lcd_putchar(48+lcd_data[4]);	
-			lcd_putchar(48+lcd_data[5]);	   
-		       lcd_putsf("hPa");/* show "hPa"*/	
-
-
-            		 /* BMP085 Altitude */                    
-	            	lcd_gotoxy(0,2);
-	            	lcd_putsf("Alt:");    
-			/* final_altitude unit : m -> if  final_altitude == 612 Pa , 612m */
-			lcd_data[0]=((INT32S)final_altitude/1000)%10;
-			lcd_data[1]=((INT32S)final_altitude/100)%10;	
-			lcd_data[2]=((INT32S)final_altitude/10)%10;
-			lcd_data[3]=((INT32S)final_altitude)%10;
-			lcd_data[4]=(INT32S)(final_altitude*10)%10;
-			lcd_data[5]=(INT32S)(final_altitude*100)%10;
-			/* show final_altitude data on LCD */
-			lcd_putchar(48+lcd_data[0]);
-			lcd_putchar(48+lcd_data[1]);
-			lcd_putchar(48+lcd_data[2]);
-		       lcd_putchar(48+lcd_data[3]);
-			lcd_putchar(46);	/* show "."*/	
-		       lcd_putchar(48+lcd_data[4]);	
-			lcd_putchar(48+lcd_data[5]);   
-		       lcd_putsf("m");/* show "m"*/	
-
-
-            		 /* BMP085 sea leve pressure*/                    
-	            	lcd_gotoxy(0,3);
-	            	lcd_putsf("P0:");    /* P0 -> Sea leve pressure */
-			/* final_sealevel_press unit : Pa -> if  final_sealevel_press == 99687 Pa , 996.87hPa */
-			lcd_data[0]=((INT32S)final_sealevel_press/100000);
-			lcd_data[1]=((INT32S)final_sealevel_press/10000)%10;
-			lcd_data[2]=((INT32S)final_sealevel_press/1000)%10;
-			lcd_data[3]=((INT32S)final_sealevel_press/100)%10;	
-			lcd_data[4]=((INT32S)final_sealevel_press/10)%10;
-			lcd_data[5]=((INT32S)final_sealevel_press)%10;
-			/* show final_sealevel_press data on LCD */
-			lcd_putchar(48+lcd_data[0]);
-			lcd_putchar(48+lcd_data[1]);
-			lcd_putchar(48+lcd_data[2]);
-		       lcd_putchar(48+lcd_data[3]);
-			lcd_putchar(46);	/* show "."*/	
-		       lcd_putchar(48+lcd_data[4]);	
-			lcd_putchar(48+lcd_data[5]);	   
-		       lcd_putsf("hPa");/* show "hPa"*/		
+			/* Get the Temperature & Pressure */
+			check_flag_BMP180 = BMP180_GET_TEMP_PRESS(&final_temperature,&final_pressure,OVERSAMPLING);
 			
-		}
-		else
-		{
-                    printf("Read BMP180 error\r\n");
-                    printf("--------------------BMP180--------------------\r\n\r\n");
-		}
-		
+			/* Calculate the altitude */		
+			BMP180_GET_ALTITUDE(final_pressure,&final_altitude);
+
+			/* Calculate the sea level pressure */
+			BMP180_GET_SEALEVEL_PRESSURE(final_altitude,final_pressure,&final_sealevel_press);
+
+
+			/* print result */
+			if(check_flag_BMP180==0)
+			{
+	                    printf("Temperature : %d.%d C\r\n",final_temperature/10,final_temperature%10);
+	                    printf("Pressure    : %ld Pa (%ld.%ld hPa)\r\n",(INT32S)final_pressure,(INT32S)final_pressure/100,(INT32S)final_pressure%100);
+	                    printf("Altitude    : %f m\r\n",final_altitude);
+	                    printf("Sea leve press   : %f Pa\r\n",final_sealevel_press);
+	                    printf("--------------------BMP180--------------------\r\n\r\n");
+
+
+	            		 /* BMP180 temperature */                    
+		            	lcd_gotoxy(0,0);
+		            	lcd_putsf("Temp:");     
+
+		           	if(final_temperature<0)
+		            	{
+		            		lcd_putchar(45);
+				}                             
+		            	else
+				{
+					lcd_putchar(32);
+				}
+						
+				/* final_temperature unit : 0.1 C -> if  final_temperature == 245 , 24.5 C */
+	 			lcd_temp[0]=final_temperature/100;
+				lcd_temp[1]=(final_temperature/10)%10;
+				lcd_temp[2]=final_temperature%10;
+				/* show final_temperature data on LCD */
+				lcd_putchar(48+lcd_temp[0]);
+			       lcd_putchar(48+lcd_temp[1]);
+				lcd_putchar(46);	/* show "."*/	
+			       lcd_putchar(48+lcd_temp[2]);
+			       lcd_putchar(67);	/* show "C"*/	
+
+
+	            		 /* BMP085 Pressure */                    
+		            	lcd_gotoxy(0,1);
+		            	lcd_putsf("P:");    
+				/* final_pressure unit : Pa -> if  final_pressure == 99687 Pa , 996.87hPa */
+				lcd_data[0]=((INT32S)final_pressure/100000);
+				lcd_data[1]=((INT32S)final_pressure/10000)%10;
+				lcd_data[2]=((INT32S)final_pressure/1000)%10;
+				lcd_data[3]=((INT32S)final_pressure/100)%10;	
+				lcd_data[4]=((INT32S)final_pressure/10)%10;
+				lcd_data[5]=((INT32S)final_pressure)%10;
+				/* show final_pressure data on LCD */
+				lcd_putchar(48+lcd_data[0]);
+				lcd_putchar(48+lcd_data[1]);
+				lcd_putchar(48+lcd_data[2]);
+			       lcd_putchar(48+lcd_data[3]);
+				lcd_putchar(46);	/* show "."*/	
+			       lcd_putchar(48+lcd_data[4]);	
+				lcd_putchar(48+lcd_data[5]);	   
+			       lcd_putsf("hPa");/* show "hPa"*/	
+
+
+	            		 /* BMP085 Altitude */                    
+		            	lcd_gotoxy(0,2);
+		            	lcd_putsf("Alt:");    
+				/* final_altitude unit : m -> if  final_altitude == 612 Pa , 612m */
+				lcd_data[0]=((INT32S)final_altitude/1000)%10;
+				lcd_data[1]=((INT32S)final_altitude/100)%10;	
+				lcd_data[2]=((INT32S)final_altitude/10)%10;
+				lcd_data[3]=((INT32S)final_altitude)%10;
+				lcd_data[4]=(INT32S)(final_altitude*10)%10;
+				lcd_data[5]=(INT32S)(final_altitude*100)%10;
+				/* show final_altitude data on LCD */
+				lcd_putchar(48+lcd_data[0]);
+				lcd_putchar(48+lcd_data[1]);
+				lcd_putchar(48+lcd_data[2]);
+			       lcd_putchar(48+lcd_data[3]);
+				lcd_putchar(46);	/* show "."*/	
+			       lcd_putchar(48+lcd_data[4]);	
+				lcd_putchar(48+lcd_data[5]);   
+			       lcd_putsf("m");/* show "m"*/	
+
+
+	            		 /* BMP085 sea leve pressure*/                    
+		            	lcd_gotoxy(0,3);
+		            	lcd_putsf("P0:");    /* P0 -> Sea leve pressure */
+				/* final_sealevel_press unit : Pa -> if  final_sealevel_press == 99687 Pa , 996.87hPa */
+				lcd_data[0]=((INT32S)final_sealevel_press/100000);
+				lcd_data[1]=((INT32S)final_sealevel_press/10000)%10;
+				lcd_data[2]=((INT32S)final_sealevel_press/1000)%10;
+				lcd_data[3]=((INT32S)final_sealevel_press/100)%10;	
+				lcd_data[4]=((INT32S)final_sealevel_press/10)%10;
+				lcd_data[5]=((INT32S)final_sealevel_press)%10;
+				/* show final_sealevel_press data on LCD */
+				lcd_putchar(48+lcd_data[0]);
+				lcd_putchar(48+lcd_data[1]);
+				lcd_putchar(48+lcd_data[2]);
+			       lcd_putchar(48+lcd_data[3]);
+				lcd_putchar(46);	/* show "."*/	
+			       lcd_putchar(48+lcd_data[4]);	
+				lcd_putchar(48+lcd_data[5]);	   
+			       lcd_putsf("hPa");/* show "hPa"*/		
+				
+			}
+			else
+			{
+	                    printf("Read BMP180 error\r\n");
+	                    printf("--------------------BMP180--------------------\r\n\r\n");
+			}
+		}	
 }
 #endif		//#ifndef __BMP180_EXAMPLE__
--- Source/SENSOR_BMP180.c
+++ Source/SENSOR_BMP180.c
@@ -5,32 +5,20 @@
      Function		: SENSOR_BMP180
      Create Date	: 2017/03/21
 ---------------------------------------------------------------------- */
-#ifndef __BMP180_FUNCTION__
-#define __BMP180_FUNCTION__
 
 #include <stdio.h>
 #include <math.h>
 #include <delay.h>
+#include <datatype_Layer.h>
+#include <swi2c_Layer.h>
+#include <SENSOR_BMP180.h>
 
-#include "SENSOR_BMP180.h"
-#include "Porting_Layer.h"
+#define SENSOR_BMP180_DEBUG    			(0)	/* open printf result value */
 
-/* define BMP180 parameter */
- struct{
-        INT16S AC1;
-        INT16S AC2;
-        INT16S AC3;
-        INT16S AC4;
-        INT16S AC5;
-        INT16S AC6;
-        INT16S B1;
-        INT16S B2;
-        INT16S MB;
-        INT16S MC;
-        INT16S MD;
-        }BMP180_CAL_COFF;
+BMP180_CAL_COFF BMP180_CAL_COFF_DATA;
 
-
+/********************************************** SYSTEM *************************************************/
+/*--------------------------------------------------------------------------------------------------*/
 /* Get the Calibration coefficients */
 void BMP180_GET_CAL_COEFF(void)
 {
@@ -38,61 +26,64 @@ void BMP180_GET_CAL_COEFF(void)
        INT16U count;
        CHAR8S fail_flag=0;
 
-        for(count=0;count<CAL_COEFF_NUMBER;count++)
-        {
-            fail_flag = i2c_read_1_byte_data(BMP180_SLAVE_ADDRESS,(0xAA+count),&BMP_CAL_COFF_ARRAY[count]);
-            if(fail_flag<=0)
-                    {
-                     printf("read error in CAL_COEFF_NUMBER[%d] \r\n",count);
-                     break;
-                    }
-        }
+	        for(count=0;count<CAL_COEFF_NUMBER;count++)
+	        {
 
-        //calculating Calibration Coefficients
-        BMP180_CAL_COFF.AC1 = (INT16S) ( (BMP_CAL_COFF_ARRAY[0]<<8) + BMP_CAL_COFF_ARRAY[1]);
-        BMP180_CAL_COFF.AC2 = (INT16S) ( (BMP_CAL_COFF_ARRAY[2]<<8) + BMP_CAL_COFF_ARRAY[3]);
-        BMP180_CAL_COFF.AC3 = (INT16S) ( (BMP_CAL_COFF_ARRAY[4]<<8) + BMP_CAL_COFF_ARRAY[5]);
-        BMP180_CAL_COFF.AC4 = (INT16S) ( (BMP_CAL_COFF_ARRAY[6]<<8) + BMP_CAL_COFF_ARRAY[7]);
-        BMP180_CAL_COFF.AC5 = (INT16S) ( (BMP_CAL_COFF_ARRAY[8]<<8) + BMP_CAL_COFF_ARRAY[9]);
-        BMP180_CAL_COFF.AC6 = (INT16S) ( (BMP_CAL_COFF_ARRAY[10]<<8) + BMP_CAL_COFF_ARRAY[11]);
-        BMP180_CAL_COFF.B1 =  (INT16S) ( (BMP_CAL_COFF_ARRAY[12]<<8) + BMP_CAL_COFF_ARRAY[13]);
-        BMP180_CAL_COFF.B2 =  (INT16S) ( (BMP_CAL_COFF_ARRAY[14]<<8) + BMP_CAL_COFF_ARRAY[15]);
-        BMP180_CAL_COFF.MB =  (INT16S) ( (BMP_CAL_COFF_ARRAY[16]<<8) + BMP_CAL_COFF_ARRAY[17]);
-        BMP180_CAL_COFF.MC =  (INT16S) ( (BMP_CAL_COFF_ARRAY[18]<<8) + BMP_CAL_COFF_ARRAY[19]);
-        BMP180_CAL_COFF.MD =  (INT16S) ( (BMP_CAL_COFF_ARRAY[20]<<8) + BMP_CAL_COFF_ARRAY[21]);
+			fail_flag = i2c_read_1_byte_data(BMP180_SLAVE_ADDRESS,(0xAA+count),&BMP_CAL_COFF_ARRAY[count]);
+			if(fail_flag<=0)
+			{
+				printf("read error in CAL_COEFF_NUMBER[%d] \r\n",count);
+	                     break;
+			}
+	        }
 
-#if sensor_bmp180_debug
+	       /*calculating Calibration Coefficients*/
+	        BMP180_CAL_COFF_DATA.AC1 = (INT16S) ( (BMP_CAL_COFF_ARRAY[0]<<8) + BMP_CAL_COFF_ARRAY[1]);
+	        BMP180_CAL_COFF_DATA.AC2 = (INT16S) ( (BMP_CAL_COFF_ARRAY[2]<<8) + BMP_CAL_COFF_ARRAY[3]);
+	        BMP180_CAL_COFF_DATA.AC3 = (INT16S) ( (BMP_CAL_COFF_ARRAY[4]<<8) + BMP_CAL_COFF_ARRAY[5]);
+	        BMP180_CAL_COFF_DATA.AC4 = (INT16S) ( (BMP_CAL_COFF_ARRAY[6]<<8) + BMP_CAL_COFF_ARRAY[7]);
+	        BMP180_CAL_COFF_DATA.AC5 = (INT16S) ( (BMP_CAL_COFF_ARRAY[8]<<8) + BMP_CAL_COFF_ARRAY[9]);
+	        BMP180_CAL_COFF_DATA.AC6 = (INT16S) ( (BMP_CAL_COFF_ARRAY[10]<<8) + BMP_CAL_COFF_ARRAY[11]);
+	        BMP180_CAL_COFF_DATA.B1 =  (INT16S) ( (BMP_CAL_COFF_ARRAY[12]<<8) + BMP_CAL_COFF_ARRAY[13]);
+	        BMP180_CAL_COFF_DATA.B2 =  (INT16S) ( (BMP_CAL_COFF_ARRAY[14]<<8) + BMP_CAL_COFF_ARRAY[15]);
+	        BMP180_CAL_COFF_DATA.MB =  (INT16S) ( (BMP_CAL_COFF_ARRAY[16]<<8) + BMP_CAL_COFF_ARRAY[17]);
+	        BMP180_CAL_COFF_DATA.MC =  (INT16S) ( (BMP_CAL_COFF_ARRAY[18]<<8) + BMP_CAL_COFF_ARRAY[19]);
+	        BMP180_CAL_COFF_DATA.MD =  (INT16S) ( (BMP_CAL_COFF_ARRAY[20]<<8) + BMP_CAL_COFF_ARRAY[21]);
 
-         for(count=0;count<CAL_COEFF_NUMBER;count++)
-         {
-            printf("a[0x%X]=0x%X  ",(0xAA+count),BMP_CAL_COFF_ARRAY[count]);
-                if(count %3==0 && count!=0 )printf("\r\n");
-         }
-            printf("\r\n");
+#if SENSOR_BMP180_DEBUG
 
-         printf("AC1 = 0x%X(%d)\r\n", BMP180_CAL_COFF.AC1,BMP180_CAL_COFF.AC1);
-         printf("AC2 = 0x%X(%d)\r\n", BMP180_CAL_COFF.AC2,BMP180_CAL_COFF.AC2);
-         printf("AC3 = 0x%X(%d)\r\n", BMP180_CAL_COFF.AC3,BMP180_CAL_COFF.AC3);
-         printf("AC4 = 0x%X(%d)\r\n", BMP180_CAL_COFF.AC4,BMP180_CAL_COFF.AC4);
-         printf("AC5 = 0x%X(%d)\r\n", BMP180_CAL_COFF.AC5,BMP180_CAL_COFF.AC5);
-         printf("AC6 = 0x%X(%d)\r\n", BMP180_CAL_COFF.AC6,BMP180_CAL_COFF.AC6);
-         printf("B1 = 0x%X(%d)\r\n", BMP180_CAL_COFF.B1,BMP180_CAL_COFF.B1);
-         printf("B2 = 0x%X(%d)\r\n", BMP180_CAL_COFF.B2,BMP180_CAL_COFF.B2);
-         printf("MB = 0x%X(%d)\r\n", BMP180_CAL_COFF.MB,BMP180_CAL_COFF.MB);
-         printf("MC = 0x%X(%d)\r\n", BMP180_CAL_COFF.MC,BMP180_CAL_COFF.MC);
-         printf("MD = 0x%X(%d)\r\n", BMP180_CAL_COFF.MD,BMP180_CAL_COFF.MD);
+		for(count=0;count<CAL_COEFF_NUMBER;count++)
+		{
+			printf("a[0x%X]=0x%X  ",(0xAA+count),BMP_CAL_COFF_ARRAY[count]);
+			if(count %3==0 && count!=0 )printf("\r\n");
+		}
+		
+		printf("\r\n");
 
+         	printf("AC1 = 0x%X(%d)\r\n", BMP180_CAL_COFF_DATA.AC1,BMP180_CAL_COFF_DATA.AC1);
+         	printf("AC2 = 0x%X(%d)\r\n", BMP180_CAL_COFF_DATA.AC2,BMP180_CAL_COFF_DATA.AC2);
+         	printf("AC3 = 0x%X(%d)\r\n", BMP180_CAL_COFF_DATA.AC3,BMP180_CAL_COFF_DATA.AC3);
+         	printf("AC4 = 0x%X(%d)\r\n", BMP180_CAL_COFF_DATA.AC4,BMP180_CAL_COFF_DATA.AC4);
+         	printf("AC5 = 0x%X(%d)\r\n", BMP180_CAL_COFF_DATA.AC5,BMP180_CAL_COFF_DATA.AC5);
+         	printf("AC6 = 0x%X(%d)\r\n", BMP180_CAL_COFF_DATA.AC6,BMP180_CAL_COFF_DATA.AC6);
+         	printf("B1 = 0x%X(%d)\r\n", BMP180_CAL_COFF_DATA.B1,BMP180_CAL_COFF_DATA.B1);
+        	printf("B2 = 0x%X(%d)\r\n", BMP180_CAL_COFF_DATA.B2,BMP180_CAL_COFF_DATA.B2);
+         	printf("MB = 0x%X(%d)\r\n", BMP180_CAL_COFF_DATA.MB,BMP180_CAL_COFF_DATA.MB);
+         	printf("MC = 0x%X(%d)\r\n", BMP180_CAL_COFF_DATA.MC,BMP180_CAL_COFF_DATA.MC);
+         	printf("MD = 0x%X(%d)\r\n", BMP180_CAL_COFF_DATA.MD,BMP180_CAL_COFF_DATA.MD);
 #endif
-}
 
+}
+/*--------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------*/
 /* Get the temperature & pressure raw data */
 CHAR8S BMP180_GET_TEMP_PRESS(INT32S *temperature_data,FLOAT *pressure_data,CHAR8U oss)
 {
         CHAR8S fail_flag=0;
 		
         INT32S X1,X2,X3,B5,REAL_TEMPUTRE;
-        INT32S B3,B6,B7;
-        INT32U B4;
+        INT32S B3,B6;
+        INT32U B4,B7;
 
         INT32S Temp_value;
         INT32S Press_value;
@@ -105,151 +96,171 @@ CHAR8S BMP180_GET_TEMP_PRESS(INT32S *temperature_data,FLOAT *pressure_data,CHAR8
         FLOAT  p_float;
 
       
-	/* temperature measurement */
-	fail_flag = i2c_write_1_byte_data(BMP180_SLAVE_ADDRESS,0XF4,0X2E);
-		if(fail_flag == SWI2C_STATUS_FAIL)  return -1;
+		/* temperature measurement */
+		fail_flag = i2c_write_1_byte_data(BMP180_SLAVE_ADDRESS,0XF4,0X2E);
+		if(fail_flag == SWI2C_STATUS_FAIL) 
+		{
+			return -1;
+		}
+		
+        	delay_ms(6);	/* wait for BMP180 conversion is finished  see spec page.17 */ 
 
-        delay_ms(6);	/* wait for BMP180 conversion is finished  see spec page.17 */ 
+		/* read tempreature data */
+		fail_flag = i2c_read_1_byte_data(BMP180_SLAVE_ADDRESS,0XF6,&high_byte_temp);
+		if(fail_flag == SWI2C_STATUS_FAIL)
+		{
+			return -1;
+		}
 
-	/* read tempreature data */
-	fail_flag = i2c_read_1_byte_data(BMP180_SLAVE_ADDRESS,0XF6,&high_byte_temp);
-		if(fail_flag == SWI2C_STATUS_FAIL)  return -1;
+		fail_flag = i2c_read_1_byte_data(BMP180_SLAVE_ADDRESS,0XF7,&low_byte_temp);
+		if(fail_flag == SWI2C_STATUS_FAIL)
+		{
+			return -1;
+		}
 
-	fail_flag = i2c_read_1_byte_data(BMP180_SLAVE_ADDRESS,0XF7,&low_byte_temp);
-		if(fail_flag == SWI2C_STATUS_FAIL)  return -1;
-
-	delay_us(30);/* delay small time */
+		delay_us(30);/* delay small time */
   
      
-       /* pressure measurement */
-	fail_flag = i2c_write_1_byte_data(BMP180_SLAVE_ADDRESS,0XF4, 0X34 + (CHAR8U)(oss<< 6) );  /* oss -> set the BMP085 measurement pressure mode */
-		if(fail_flag == SWI2C_STATUS_FAIL)  return -1;
+       	/* pressure measurement */
+		fail_flag = i2c_write_1_byte_data(BMP180_SLAVE_ADDRESS,0XF4, 0X34 + (CHAR8U)(oss<< 6) );  /* oss -> set the BMP085 measurement pressure mode */
+		if(fail_flag == SWI2C_STATUS_FAIL) 
+		{
+			return -1;
+		}
+		
+       	switch(oss)	/* wait for BMP180 conversion is finished  see spec page.17 */ 
+        	{
+         		case 0:/* datasheet max time :4.5ms */
+					delay_ms(6);
+					break;
+         		case 1:/*datasheet max time :7.5ms */
+					delay_ms(9);
+					break;
+         		case 2:/*datasheet max time :13.5ms */
+					delay_ms(15);
+					break;
+         		case 3:/*datasheet max time :25.5ms */
+					delay_ms(27);
+					break;
+         		default:
+					return -1;  /*wrong value*/
+        	}
 
-       switch(oss)	/* wait for BMP180 conversion is finished  see spec page.17 */ 
-        {
-         case 0:/* datasheet max time :4.5ms */
-				delay_ms(6);
-			break;
-         case 1:/*datasheet max time :7.5ms */
-				delay_ms(9);
-			break;
-         case 2:/*datasheet max time :13.5ms */
-				delay_ms(15);
-			break;
-         case 3:/*datasheet max time :25.5ms */
-				delay_ms(27);
-			break;
-         default:
-			return -1;  //wrong value
-        }
+		/* read pressure data */
+		fail_flag = i2c_read_1_byte_data(BMP180_SLAVE_ADDRESS,0XF6,&high_byte_press);
+		if(fail_flag == SWI2C_STATUS_FAIL)  
+		{
+			return -1;
+		}
 
-	/* read pressure data */
-	fail_flag = i2c_read_1_byte_data(BMP180_SLAVE_ADDRESS,0XF6,&high_byte_press);
-		if(fail_flag == SWI2C_STATUS_FAIL)  return -1;
+		fail_flag = i2c_read_1_byte_data(BMP180_SLAVE_ADDRESS,0XF7,&low_byte_press);
+		if(fail_flag == SWI2C_STATUS_FAIL)  
+		{
+			return -1;
+		}
 
-	fail_flag = i2c_read_1_byte_data(BMP180_SLAVE_ADDRESS,0XF7,&low_byte_press);
-             if(fail_flag == SWI2C_STATUS_FAIL)  return -1;
+		fail_flag = i2c_read_1_byte_data(BMP180_SLAVE_ADDRESS,0XF8,&x_low_byte_press);
+		if(fail_flag == SWI2C_STATUS_FAIL)
+		{
+			return -1;
+		}
 
-	fail_flag = i2c_read_1_byte_data(BMP180_SLAVE_ADDRESS,0XF8,&x_low_byte_press);
-             if(fail_flag == SWI2C_STATUS_FAIL)  return -1;
+		/* Calculation Tempearture */
+		Temp_value = (INT32S) ((high_byte_temp << 8) + low_byte_temp);
 
-
-	/* Calculation Tempearture */
-	Temp_value = (INT32S) ((high_byte_temp << 8) + low_byte_temp);
-
-	X1 = ((INT32S)Temp_value - BMP180_CAL_COFF.AC6)*(BMP180_CAL_COFF.AC5)/32768;
-	X2 =  (INT32S)((INT32S)BMP180_CAL_COFF.MC *2048)/(INT32S)(X1 + (INT32S)BMP180_CAL_COFF.MD);
-	B5 = (INT32S)(X1 + X2);
-	REAL_TEMPUTRE = (INT32S)(B5 + 8)/ 16;        /* Final Temperature , ex: 153 temp  setp in 0.1 , 15.3 oC */
+		X1 = ((INT32S)Temp_value - BMP180_CAL_COFF_DATA.AC6)*(BMP180_CAL_COFF_DATA.AC5)/32768;
+		X2 =  (INT32S)((INT32S)BMP180_CAL_COFF_DATA.MC *2048)/(INT32S)(X1 + (INT32S)BMP180_CAL_COFF_DATA.MD);
+		B5 = (INT32S)(X1 + X2);
+		REAL_TEMPUTRE = (INT32S)(B5 + 8)/ 16;        /* Final Temperature , ex: 153 temp  setp in 0.1 , 15.3 oC */
  
-     /* Calculation Pressuree */
-        pr1 = (INT32S)high_byte_press << 16;
-        pr2 = (INT32S)low_byte_press << 8;
-        pr3 = (INT32S)x_low_byte_press;
-        pr4 = (INT32S)(8-oss);
-        Press_value = (INT32S)((pr1+pr2+pr3) >> pr4);
+		/* Calculation Pressuree */
+		pr1 = (INT32S)high_byte_press << 16;
+		pr2 = (INT32S)low_byte_press << 8;
+		pr3 = (INT32S)x_low_byte_press;
+		pr4 = (INT32S)(8-oss);
+		Press_value = (INT32S)((pr1+pr2+pr3) >> pr4);
 
 
-        B6 = (INT32S)(B5 - 4000);
-        pr1 = (INT32S)(B6 * B6)/4096;
+		B6 = (INT32S)(B5 - 4000);
+		pr1 = (INT32S)(B6 * B6)/4096;
 
-        X1 = ((BMP180_CAL_COFF.B2)*pr1)/2048 ;
-        X2 = (INT32S) ((INT32S)BMP180_CAL_COFF.AC2) * B6 / 2048;
-        X3 = X1 + X2;
-        B3 =(INT32S)(((((INT32S)BMP180_CAL_COFF.AC1*4+X3)<< (INT32S)(oss) ) + 2)/4 );
-        X1 =(INT32S)(((INT32S)BMP180_CAL_COFF.AC3 * B6)/8192);
-        X2 =(INT32S)(((INT32S)BMP180_CAL_COFF.B1 * ((B6*B6)/4096))/65536);
-        X3 =(INT32S)(((X1 + X2) + 2)/4);
-        pr4 =(INT32S)(X3 + 32768);
-        pr5 =(INT32S)BMP180_CAL_COFF.AC4;
-        B4 = (INT32U) ((pr4 * pr5)/32768);
-        pr1 = (INT32S)(50000 >> (INT32S)(oss));
-        B7 = (INT32S)((INT32U)Press_value - B3) * pr1;
+		X1 = ((BMP180_CAL_COFF_DATA.B2)*pr1)/2048 ;
+		X2 = (INT32S) ((INT32S)BMP180_CAL_COFF_DATA.AC2) * B6 / 2048;
+		X3 = X1 + X2;
+		B3 =(INT32S)(((((INT32S)BMP180_CAL_COFF_DATA.AC1*4+X3)<< (INT32S)(oss) ) + 2)/4 );
+		X1 =(INT32S)(((INT32S)BMP180_CAL_COFF_DATA.AC3 * B6)/8192);
+		X2 =(INT32S)(((INT32S)BMP180_CAL_COFF_DATA.B1 * ((B6*B6)/4096))/65536);
+		X3 =(INT32S)(((X1 + X2) + 2)/4);
+		pr4 =(INT32S)(X3 + 32768);
+		pr5 =(INT32S)BMP180_CAL_COFF_DATA.AC4;
+		B4 = (INT32U) ((pr4 * pr5)/32768);
+		pr1 = (INT32S)(50000 >> (INT32S)(oss));
+		B7 = (INT32S)((INT32U)Press_value - B3) * pr1;
 		
 
-        if( B7 < (0x80000000) )
-	{
-		p_float = ((FLOAT)B7*2/(FLOAT)B4);
-	}
-        else
-	{
-             p_float = (((FLOAT)B7*(FLOAT)B4)/2);
-	}
+		if( B7 < (0x80000000) )
+		{
+			p_float = ((FLOAT)B7*2/(FLOAT)B4);
+		}
+		else
+		{
+			p_float = (((FLOAT)B7*(FLOAT)B4)/2);
+		}
 
 
-        X1 =(INT32S) ((p_float/256) *(p_float/256));
-        X1 = ((X1 *3038)/65536);
-        X2 = (INT32S)((-7357 * p_float)/65536);
-        p_float = (p_float + ((FLOAT)X1 +(FLOAT)X2 + 3791)/16); /* Final pressure ,  press. in Pa */
+		X1 =(INT32S) ((p_float/256) *(p_float/256));
+		X1 = ((X1 *3038)/65536);
+        	X2 = (INT32S)((-7357 * p_float)/65536);
+        	p_float = (p_float + ((FLOAT)X1 +(FLOAT)X2 + 3791)/16); /* Final pressure ,  press. in Pa */
 
 
-#if sensor_bmp180_debug
-        printf("Temp_value = %ld\r\n",Temp_value);
-        printf("X1 = %ld\r\n",(INT32S)X1);
-        printf("X2 = %ld\r\n",(INT32S)X2);
-        printf("B5 = %lu\r\n",(INT32S)B5);
-        printf("REAL_TEMPUTRE = %ld.%ld\r\n",(INT32S)REAL_TEMPUTRE/10,(INT32S)REAL_TEMPUTRE%10);
+#if SENSOR_BMP180_DEBUG
+		printf("Temp_value = %ld\r\n",Temp_value);
+		printf("X1 = %ld\r\n",(INT32S)X1);
+		printf("X2 = %ld\r\n",(INT32S)X2);
+        	printf("B5 = %lu\r\n",(INT32S)B5);
+        	printf("REAL_TEMPUTRE = %ld.%ld\r\n",(INT32S)REAL_TEMPUTRE/10,(INT32S)REAL_TEMPUTRE%10);
 #endif
 
 
-        /* Final parameter Set */
-        *pressure_data = (p_float);						/* Final Pressure , ex: 100053 Pa = 1000.53 hpa*/
-        *temperature_data = (INT32S)REAL_TEMPUTRE;		/* Final Temperature*/
+		/* Final parameter Set */
+		*pressure_data = (p_float);							/* Final Pressure , ex: 100053 Pa = 1000.53 hpa*/
+		*temperature_data = (INT32S)REAL_TEMPUTRE;		/* Final Temperature*/
 
-        return 0;
+		return 0;
 
 }
-
-
+/*--------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------*/
 /* Calculate the sea level pressure */
 void BMP180_GET_ALTITUDE(FLOAT pressure_data,FLOAT *altitude_result)
 {
 	FLOAT alt_temp1,alt_temp2,alt_temp3,alt_temp4,alt_temp5;
 	
-        /* Calculating absolute altitude  */
-        alt_temp1 = ((FLOAT)pressure_data/101325);
-        alt_temp2 = (1/5.255);
-        alt_temp3 = pow(alt_temp1,alt_temp2);
-        alt_temp4 = (1 - alt_temp3);
-        alt_temp5 = 44330.0 *(alt_temp4);	/*altitude result*/
-      
- 	*altitude_result = alt_temp5;		/* Final altitude*/
+	        /* Calculating absolute altitude  */
+	        alt_temp1 = ((FLOAT)pressure_data/101325);
+	        alt_temp2 = (1/5.255);
+	        alt_temp3 = pow(alt_temp1,alt_temp2);
+	        alt_temp4 = (1 - alt_temp3);
+	        alt_temp5 = 44330.0 *(alt_temp4);	/*altitude result*/
+	      
+	 	*altitude_result = alt_temp5;		/* Final altitude*/
 }
-
+/*--------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------*/
 /* Calculate the altitude */      
 void BMP180_GET_SEALEVEL_PRESSURE(FLOAT altitude,FLOAT pressure_data,FLOAT *sea_level_pressure)
 {
 	FLOAT alt_temp6,alt_temp7,alt_temp8,alt_temp9,alt_temp10;
 	
-        /* Calculating pressure at sea level  */
-        alt_temp6 = ((FLOAT)altitude/44330.0);
-        alt_temp7 = (1 - alt_temp6);
-        alt_temp8 = pow(alt_temp7,5.255);
-        alt_temp9 = (pressure_data /alt_temp8);
-        alt_temp10 = alt_temp9;      
-        
-	*sea_level_pressure = alt_temp10;		/* Final sea level*/
+	        /* Calculating pressure at sea level  */
+	        alt_temp6 = ((FLOAT)altitude/44330.0);
+	        alt_temp7 = (1 - alt_temp6);
+	        alt_temp8 = pow(alt_temp7,5.255);
+	        alt_temp9 = (pressure_data /alt_temp8);
+	        alt_temp10 = alt_temp9;      
+	        
+		*sea_level_pressure = alt_temp10;		/* Final sea level*/
 }
-      
-		
-#endif		//#ifndef __BMP180_FUNCTION__
+/*--------------------------------------------------------------------------------------------------*/
+/********************************************** SYSTEM *************************************************/
